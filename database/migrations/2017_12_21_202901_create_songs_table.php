f95c6b22c1d54cd187fa6cd00c4e19ee40b8b626
Unknown <ifjuszalaimihaly@gmail.com>
2017-12-21 21:45:25 +0100

    migrated songs

--- database/migrations/2017_12_21_202901_create_songs_table.php
+++ database/migrations/2017_12_21_202901_create_songs_table.php
@@ -14,7 +14,16 @@ class CreateSongsTable extends Migration
     public function up()
     {
         Schema::create('songs', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_hungarian_ci';
             $table->increments('id');
+            $table->smallInteger('number')->unique();
+            $table->string('title',100)->unique();
+            $table->string('title_original',100)->nullable();
+            $table->text('text');
+            $table->string('youtube',100)->nullable();
+            $table->string('sheet_music',50)->nullable();
             $table->timestamps();
         });
     }
