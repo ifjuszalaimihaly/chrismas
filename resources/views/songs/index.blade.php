b295e7492e847b5ab3ad95e0ea9974e6d4a08567
Unknown <ifjuszalaimihaly@gmail.com>
2017-12-22 16:48:44 +0100

    added new css rules for song index view

---
+++ resources/views/songs/index.blade.php
@@ -0,0 +1,53 @@
+@extends('master')
+@section('title')
+    Áldott karácsonyi ünnepeket kívánok!
+@endsection
+<body>
+
+@include('nav')
+
+
+<!-- Page Content -->
+<section class="py-5">
+    <!-- Page Heading -->
+    <h1 class="my-4">Page Heading
+        <small>Secondary Text</small>
+    </h1>
+    <div class="row">
+        <div class="row">
+            <div class="col-lg-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Project One</a>
+                        </h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra
+                            euismod odio, gravida pellentesque urna varius vitae.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 portfolio-item">
+                <div class="card h-100">
+                    <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            <a href="#">Project Two</a>
+                        </h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit aliquam
+                            aperiam nulla perferendis dolor nobis numquam, rem expedita, aliquid optio, alias illum
+                            eaque. Non magni, voluptates quae, necessitatibus unde temporibus.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+@include('footer')
+
+<script src="/js/app.js"></script>
+
+</body>
+
+</html>
